95a9fb640310e743245caa513057886a55c5c592
yoganino123 <yoganinorahardian@gmail.comm>
2022-09-20 15:18:31 +0700

    Controlers Login

--- server/controllers/home/HomeController.js
+++ server/controllers/home/HomeController.js
@@ -1,20 +1,39 @@
 const { user } = require("../../models");
+const { decryptPass } = require("../../helpers/bcrypt");
 
 class HomeController {
   static async login(req, res) {
     try {
+      const { email, password } = req.body;
+      let emailFound = await user.findOne({
+        where: { email },
+      });
+
+      if (emailFound) {
+        if (decryptPass(password, emailFound.password)) {
+          res.status(200).json(emailFound);
+        } else {
+          res.status(403).json({
+            massage: "Invalid password!",
+          });
+        }
+      } else {
+        res.status(404).json({
+          message: "Email not found!",
+        });
+      }
     } catch (err) {
       res.status(500).json(err);
     }
   }
   static async register(req, res) {
     try {
-      const { nama, email, password } = req.body;
+      const { name, email, password } = req.body;
       const valUser = await user.findOne({ where: { email } });
       if (valUser) {
         res.status(200).json({ msg: `Email sudah terdaftar!` });
       } else {
-        const addUser = await user.create({ nama, email, password });
+        const addUser = await user.create({ name, email, password });
         res.status(201).json(addUser);
       }
     } catch (err) {
