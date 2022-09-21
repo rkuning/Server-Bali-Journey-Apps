b9d02c02814f93a0208a77ff4f7ef9981e2a90d1
yoganino123 <yoganinorahardian@gmail.comm>
2022-09-21 20:00:29 +0700

    home route

--- server/routes/home/homeRoute.js
+++ server/routes/home/homeRoute.js
@@ -4,5 +4,14 @@ const { HomeController } = require("../../controllers");
 homeRoute.get("/", (req, res) => res.json("homepage"));
 homeRoute.post("/login", HomeController.login);
 homeRoute.post("/register", HomeController.register);
+homeRoute.get("/category", HomeController.category);
+homeRoute.get("/allDestinations", HomeController.allDestinations);
+homeRoute.get("/allPackageTrip", HomeController.allPackageTrip);
+homeRoute.get("/recomenDestinations", HomeController.recomenDestinations);
+homeRoute.get("/recomenPackageTrip", HomeController.recomenPackageTrip);
+homeRoute.get("/destination/:id", HomeController.destinationId);
+homeRoute.get("/packageTrip/:id", HomeController.packageTripId);
+homeRoute.get("/reviewDestination/:id", HomeController.reviewDestinationId);
+homeRoute.get("/reviewPackageTrip/:id", HomeController.reviewPackageTripId);
 
 module.exports = homeRoute;
