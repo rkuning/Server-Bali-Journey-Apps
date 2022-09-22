d7514d444e46304bf656a358b6d5968952ecfebd
Danang Ipul Basrin <danangipulbasrin@gmail.com>
2022-09-22 15:05:39 +0700

    update package trip

--- server/routes/admin/packageTripRoute.js
+++ server/routes/admin/packageTripRoute.js
@@ -4,8 +4,10 @@ const { upload } = require("../../middlewares");
 
 packageTripRoutes.get("/", PackageTripController.getPackageTrip);
 packageTripRoutes.get("/:id", PackageTripController.getPackageTripId);
+packageTripRoutes.post("/:id", PackageTripController.addPackageDestination);
 packageTripRoutes.post("/", upload.single("img"), PackageTripController.addPackageTrip);
 packageTripRoutes.delete("/:id", PackageTripController.deletePackageTrip);
+packageTripRoutes.delete("/dest/:id", PackageTripController.deletePackageDestination);
 packageTripRoutes.put("/:id", PackageTripController.updatePackageTrip);
 
 module.exports = packageTripRoutes;
